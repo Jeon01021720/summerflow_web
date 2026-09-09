7e6c50d38c3d229474da07ba98515e9e02d62768
Jeon Gyeongheun <gyeongheunjeon@gmail.com>
2026-09-10 02:39:04 +0900

    Show open diary in memory capsule build 40

---
+++ diary-open-patch.js
@@ -0,0 +1,26 @@
+(() => {
+  const OPEN_DIARY_SRC = './assets/web/diary-open.svg';
+
+  function swapOpenDiary(root = document) {
+    const image = root.querySelector?.('.sf-memory-diary');
+    if (!image || image.dataset.sfOpenDiary === '1') return;
+    image.dataset.sfOpenDiary = '1';
+    image.src = OPEN_DIARY_SRC;
+    image.alt = '';
+    image.setAttribute('aria-hidden', 'true');
+  }
+
+  swapOpenDiary();
+
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (!(node instanceof Element)) continue;
+        if (node.matches?.('.sf-memory-diary')) swapOpenDiary(node.parentElement || node);
+        else swapOpenDiary(node);
+      }
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
